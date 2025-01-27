46bf47eefba40cc605e9294f0bfdfe4230c0cb2c
Nirina <111906067+nirina-git@users.noreply.github.com>
2025-01-27 15:46:30 +0400

    ajout d'un workflow de déploiement github pages et mise a jour de la configuration du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nirina-git.github.io',
+  base: 'cv',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
