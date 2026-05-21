10e3dd229e221540cb2006de2e6377d61e63b154
Nirina <111906067+nirina-git@users.noreply.github.com>
2026-05-21 23:08:08 +0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://nirina-git.github.io',
-  base: 'cv',
+  site: 'https://cv.rmnjs.org',
+  base: '/',
   trailingSlash: 'always',
   integrations: [tailwind()],
 });
